5ecd8798895e0881aec39ffe351e15c46a436a50
Gabriel Collin <gabicollin91910@gmail.com>
2025-11-07 21:51:07 +0100

    Sort the provided files

---
+++ POO/Prosit 1/L1-Segments2D-C/build/CMakeFiles/Exercice.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/shaden/Cesi/CPIA2/Bloque/Cesi/POO/Prosit 1/L1-Segments2D-C/src/Point2.c" "CMakeFiles/Exercice.dir/src/Point2.c.o" "gcc" "CMakeFiles/Exercice.dir/src/Point2.c.o.d"
+  "/home/shaden/Cesi/CPIA2/Bloque/Cesi/POO/Prosit 1/L1-Segments2D-C/src/Segment2.c" "CMakeFiles/Exercice.dir/src/Segment2.c.o" "gcc" "CMakeFiles/Exercice.dir/src/Segment2.c.o.d"
+  "/home/shaden/Cesi/CPIA2/Bloque/Cesi/POO/Prosit 1/L1-Segments2D-C/src/Vector2.c" "CMakeFiles/Exercice.dir/src/Vector2.c.o" "gcc" "CMakeFiles/Exercice.dir/src/Vector2.c.o.d"
+  "/home/shaden/Cesi/CPIA2/Bloque/Cesi/POO/Prosit 1/L1-Segments2D-C/src/main.c" "CMakeFiles/Exercice.dir/src/main.c.o" "gcc" "CMakeFiles/Exercice.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ POO/Prosit 1/L1-Segments2D-C/build/CMakeFiles/Exercice.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercice.dir/src/Point2.c.o"
+  "CMakeFiles/Exercice.dir/src/Point2.c.o.d"
+  "CMakeFiles/Exercice.dir/src/Segment2.c.o"
+  "CMakeFiles/Exercice.dir/src/Segment2.c.o.d"
+  "CMakeFiles/Exercice.dir/src/Vector2.c.o"
+  "CMakeFiles/Exercice.dir/src/Vector2.c.o.d"
+  "CMakeFiles/Exercice.dir/src/main.c.o"
+  "CMakeFiles/Exercice.dir/src/main.c.o.d"
+  "Exercice"
+  "Exercice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
