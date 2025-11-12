e90bb20d887cb5c8d5745b18821609be5a026357
gabriel Collin <gabicollin91910@gmail.com>
2025-11-12 14:00:07 +0100

    del build

--- POO/Prosit 1/Main/src/main.cpp
+++ POO/Prosit 1/Main/src/main.cpp
@@ -2,11 +2,12 @@
 #include "vector.hpp"
 #include "segment.hpp"
 #include <cstdlib>
+#include <iostream>
 
 int main() {
   Point p1(0.,0.), p2(1.,2.);
   Vector v1(1.,1.), v2(2.,4.);
   Segment s1(p1,v1), s2(p1,v1);
-
+  std::cout << "Code ok";
   return EXIT_SUCCESS;
 }
